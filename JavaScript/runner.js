7823baf08e58f06cef2b710b8fb94102095703b7
RathaR <ivan.pecharitsa@gmail.com>
2015-04-19 22:02:15 +0300

    read all cases from tests.json if command line args are not specified

--- JavaScript/runner.js
+++ JavaScript/runner.js
@@ -4,23 +4,28 @@ var loader = require('./solution-loader');
 
 var arguments = process.argv.slice(2);
 
-var problemId = _.first(arguments);
-//
-//if(_.isUndefined(problemId)) {
-//
-//}
-var testInfo = tests[problemId];
-var solution = loader.load(problemId);
-var entryPoint = testInfo.entryPoint;
-var problemSpecs = testInfo.specs;
-console.log('start executing tests for : ' + problemId);
-console.log('entry point: ' + entryPoint);
-_.forEach(problemSpecs, function (spec) {
-    console.log('test id : ' + spec.id);
-    console.log('   input : ' + spec.input);
-    console.log('   expect : ' + spec.expect);
-    var result = solution.run(entryPoint, spec.input, spec.expected);
-    console.log('   get : ', result);
-    var testResult = result == spec.expect;
-    console.log('   Result: ' + testResult);
+var problemsList = [];
+if (arguments.length > 0) {
+    problemsList = arguments;
+} else {
+    problemsList = _.keys(tests);
+}
+_.forEach(problemsList, function (problemId) {
+    var testInfo = tests[problemId];
+    var solution = loader.load(problemId);
+    var entryPoint = testInfo.entryPoint;
+    var problemSpecs = testInfo.specs;
+    console.log('start executing tests for : ' + problemId);
+    console.log('entry point: ' + entryPoint);
+    _.forEach(problemSpecs, function (spec) {
+        console.log('test id : ' + spec.id);
+        console.log('   input : ' + spec.input);
+        console.log('   expect : ' + spec.expect);
+        var result = solution.run(entryPoint, spec.input, spec.expected);
+        console.log('   get : ', result);
+        var testResult = result == spec.expect;
+        console.log('   Result: ' + testResult);
+    });
 });
+
+
