e0e923f655d415fb5d7899bcbeaee6ecab59efc3
RathaR <ivan.pecharitsa@gmail.com>
2015-04-21 00:28:35 +0300

    fix loader for new project structure

--- JavaScript/runner.js
+++ JavaScript/runner.js
@@ -1,29 +1,12 @@
 var _ = require('lodash');
 var loader = require('./solution-loader');
 
-var arguments = process.argv.slice(2);
+//var arguments = process.argv.slice(2);
 
 var problemsList = loader.loadAll();
-//var problemsList = [];
-//if (arguments.length > 0) {
-//    problemsList = arguments;
-//} else {
-//    problemsList = _.keys(tests);
-//}
-_.forEach(problemsList, function (solution, problemId) {
-    var entryPoint = testInfo.entryPoint;
-    var problemSpecs = testInfo.specs;
-    console.log('start executing tests for : ' + problem);
-    console.log('entry point: ' + entryPoint);
-    _.forEach(problemSpecs, function (spec) {
-        console.log('test id : ' + spec.id);
-        console.log('   input : ' + spec.input);
-        console.log('   expect : ' + spec.expect);
-        var result = solution.run(entryPoint, spec.input, spec.expected);
-        console.log('   get : ', result);
-        var testResult = result == spec.expect;
-        console.log('   Result: ' + testResult);
-    });
+
+_.forEach(problemsList, function (solution) {
+    solution.test();
 });
 
 
--- JavaScript/solution-loader.js
+++ JavaScript/solution-loader.js
@@ -1,25 +1,45 @@
 var fs = require('fs');
+var _ = require('lodash');
+
 var Solution = function (params) {
     this.problemName = params.problemName;
     this.entryPoint = params.entryPoint;
     this.code = params.code;
     this.specs = params.specs;
 };
+Solution.prototype.test = function () {
+    var self = this;
+    if (self.specs.length === 0) {
+        console.log('for ' + self.problemName + ' specs not found!');
+        return;
+    }
+    console.log('test : ' + self.problemName);
+    _.forEach(this.specs, function (spec) {
+        console.log('start executing spec: ' + spec.id);
+        console.log('   input : ' + spec.input);
+        console.log('   expect : ' + spec.expect);
+        var result = self.run(spec.input);
+        console.log('   result : ' + result);
+        var status = _.isEqual(result, spec.expect);
+        console.log(status ? 'SUCCESS' : 'FAIL');
+    });
+};
 Solution.prototype.run = function (input) {
     var result = null;
+    var self = this;
     var evaluationContext = {};
     (function () {
         with (evaluationContext) {
-            eval(this.code);
-            result = eval(this.entryPoint + '(\'' + input + '\');')
+            eval(self.code);
+            result = eval(self.entryPoint + '(\'' + input + '\');')
         }
     })();
     return result;
 };
 var loadFunc = function (problemId) {
-    var code = fs.readFileSync('solutions/' + problemId + '.js', {encoding: 'utf-8'});
-    var data = fs.readFileSync('solutions/' + problemId + '.json', {encoding: 'utf-8'});
-    return new Solution({ code: code, entryPoint: data.entryPoint, specs: data.specs, problemName: problemId});
+    var code = fs.readFileSync('solutions/' + problemId + '/' + problemId + '.js', {encoding: 'utf-8'});
+    var data = JSON.parse(fs.readFileSync('solutions/' + problemId + '/' + problemId + '.json', {encoding: 'utf-8'}));
+    return new Solution({code: code, entryPoint: data.entryPoint, specs: data.specs, problemName: problemId});
 };
 module.exports = {
     load: loadFunc,
